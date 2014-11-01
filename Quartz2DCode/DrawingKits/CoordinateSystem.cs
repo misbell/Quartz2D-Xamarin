6002a0c15f0f364d529a322362ec5c1bf4308d40
prenez <>
2014-11-01 01:09:11 -0400

    updates

--- Quartz2DCode/DrawingKits/CoordinateSystem.cs
+++ Quartz2DCode/DrawingKits/CoordinateSystem.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+using CoreFoundation;
 
 namespace Quartz2DCode
 {
@@ -8,33 +12,64 @@ namespace Quartz2DCode
 		{
 		}
 
-		void doRotatedEllipses(CGContextRef context)
+	//	void doRotatedEllipses(CGContextRef context)
+		void doRotatedEllipses()
 		{
 			int i, totreps = 144;
 			float  tint = 1.0, tintIncrement = 1.0/totreps;
 			// Create a new transform consisting of a 45 degrees rotation.
-			CGAffineTransform theTransform = CGAffineTransformMakeRotation(M_PI/4);
+
+			NSGraphicsContext ccontext = NSGraphicsContext.CurrentContext;
+			CGContext context = NSGraphicsContext.CurrentContext.GraphicsPort;
+
+			CGAffineTransform theTransform = CGAffineTransform.MakeRotation(Math.PI/4.0f);
+			//CGAffineTransform theTransform = CGAffineTransformMakeRotation(M_PI/4);
+
 			// Apply a scale to the transform just created.
-			theTransform = CGAffineTransformScale(theTransform, 1, 2);
+
+			//theTransform = CGAffineTransformScale(theTransform, 1, 2);
+			theTransform = CGAffineTransform.MakeScale(1.0f, 2.0f);
+
 			// Place the first ellipse at a good location.
-			CGContextTranslateCTM(context, 100., 100.);
+			//CGContextTranslateCTM(context, 100., 100.);
+			context.TranslateCTM(100.0, 100.0);
 
 			for(i=0 ; i < totreps ; i++){
 				// Make a snapshot the coordinate system.
-				CGContextSaveGState(context);
+
+				//CGContextSaveGState(context);
+				context.SaveState();
+
 				// Set up the coordinate system for the rotated ellipse.
-				CGContextConcatCTM(context, theTransform);
-				CGContextBeginPath(context);
-				CGContextAddArc(context, 0., 0., 45., 0., 2*M_PI, 0); 
+
+				//CGContextConcatCTM(context, theTransform);
+				context.ConcatCTM(theTransform);
+
+				//CGContextBeginPath(context);
+				context.BeginPath();
+
+
+				//CGContextAddArc(context, 0., 0., 45., 0., 2*M_PI, 0);
+				context.AddArc(0.0f, 0.0f, 45.0f, 0.0f, 2.0f * Math.PI, 0.0f);
+
+
 				// Set the fill color for this instance of the ellipse.
-				CGContextSetRGBFillColor(context, tint, 0., 0., 1.0);
-				CGContextDrawPath(context, kCGPathFill);
+				//CGContextSetRGBFillColor(context, tint, 0., 0., 1.0);
+				context.SetFillColor(tint,0.0f, 0.0f, 1.0f);
+
+
+				//CGContextDrawPath(context, kCGPathFill);
+				context.DrawPath(CGPathDrawingMode.Fill);
+
 				// Restore the coordinate system to that of the snapshot.
-				CGContextRestoreGState(context);
+				//CGContextRestoreGState(context);
+				context.RestoreState();
+
 				// Compute the next tint color.
 				tint -= tintIncrement;
 				// Move over by 1 unit in x for the next ellipse.
-				CGContextTranslateCTM(context, 1.0, 0.0);
+				//CGContextTranslateCTM(context, 1.0, 0.0);
+				context.TranslateCTM(1.0f, 0.0f);
 			}
 		}
 
