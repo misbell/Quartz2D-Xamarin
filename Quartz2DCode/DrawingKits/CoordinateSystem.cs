3b4a85fcfbef7476c2070addd39927e1de84d619
prenez <>
2014-11-01 02:07:11 -0400

    from ide

--- Quartz2DCode/DrawingKits/CoordinateSystem.cs
+++ Quartz2DCode/DrawingKits/CoordinateSystem.cs
@@ -13,16 +13,16 @@ namespace Quartz2DCode
 		}
 
 	//	void doRotatedEllipses(CGContextRef context) //
-		void doRotatedEllipses()
+		public void doRotatedEllipses()
 		{
 			int i, totreps = 144;
-			float  tint = 1.0, tintIncrement = 1.0/totreps;
+			float  tint = 1.0f, tintIncrement = 1.0f/totreps;
 			// Create a new transform consisting of a 45 degrees rotation.
 
 			NSGraphicsContext ccontext = NSGraphicsContext.CurrentContext;
 			CGContext context = NSGraphicsContext.CurrentContext.GraphicsPort;
 
-			CGAffineTransform theTransform = CGAffineTransform.MakeRotation(Math.PI/4.0f);
+			CGAffineTransform theTransform = CGAffineTransform.MakeRotation((nfloat)(Math.PI/4.0f));
 			//CGAffineTransform theTransform = CGAffineTransformMakeRotation(M_PI/4);
 
 			// Apply a scale to the transform just created.
@@ -32,7 +32,7 @@ namespace Quartz2DCode
 
 			// Place the first ellipse at a good location.
 			//CGContextTranslateCTM(context, 100., 100.);
-			context.TranslateCTM(100.0, 100.0);
+			context.TranslateCTM(100.0f, 100.0f);
 
 			for(i=0 ; i < totreps ; i++){
 				// Make a snapshot the coordinate system.
@@ -50,7 +50,7 @@ namespace Quartz2DCode
 
 
 				//CGContextAddArc(context, 0., 0., 45., 0., 2*M_PI, 0);
-				context.AddArc(0.0f, 0.0f, 45.0f, 0.0f, 2.0f * Math.PI, 0.0f);
+				context.AddArc(0.0f, 0.0f, 45.0f, 0.0f, (nfloat)(2.0f * Math.PI), false);
 
 
 				// Set the fill color for this instance of the ellipse.
@@ -73,16 +73,25 @@ namespace Quartz2DCode
 			}
 		}
 
-		void drawSkewedCoordinateSystem(CGContextRef context)
+		//void drawSkewedCoordinateSystem(CGContextRef context)
+		public void drawSkewedCoordinateSystem()
 		{
+
+			NSGraphicsContext ccontext = NSGraphicsContext.CurrentContext;
+			CGContext context = NSGraphicsContext.CurrentContext.GraphicsPort;
+
 			// alpha is 22.5 degrees and beta is 15 degrees.
-			float alpha = M_PI/8, beta = M_PI/12;
+			//float alpha =  M_PI/8, beta = M_PI/12;
+			float alpha = Math.PI / 8.0f;
+			float beta = Math.PI / 12.0f;
 			CGAffineTransform skew;
 			// Create a rectangle that is 72 units on a side
 			// with its origin at (0,0).
-			CGRect r = CGRectMake(0, 0, 72, 72);
+			//CGRect r = CGRectMake(0, 0, 72, 72);
 
-			CGContextTranslateCTM(context, 144, 144);
+			//CGContextTranslateCTM(context, 144, 144);
+			context.TranslateCTM(144.0f, 144.0f);
+
 			// Draw the coordinate axes untransformed.
 			drawCoordinateAxes(context);
 			// Fill the rectangle.
--- Quartz2DCode/DrawingKits/Utilities.cs
+++ Quartz2DCode/DrawingKits/Utilities.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+using CoreFoundation;
 
 namespace Quartz2DCode
 {
@@ -7,6 +11,69 @@ namespace Quartz2DCode
 		public Utilities ()
 		{
 		}
+
+		void drawCoordinateAxes(CGContextRef context)
+		{
+			int i;
+			float t;
+			float tickLength = kTickLength;
+
+			// work with the current context, associated with view
+			NSGraphicsContext ccontext = NSGraphicsContext.CurrentContext;
+			CGContext context = NSGraphicsContext.CurrentContext.GraphicsPort;
+
+			//CGContextSaveGState(context);
+			context.
+
+			CGContextBeginPath(context);
+			// Paint the x-axis in red.
+			CGContextSetRGBStrokeColor(context, 1, 0, 0, 1);
+			CGContextMoveToPoint(context, -kTickLength, 0.);
+			CGContextAddLineToPoint(context, kAxesLength, 0.);
+			CGContextDrawPath(context, kCGPathStroke);
+
+			// Paint the y-axis in blue.
+			CGContextSetRGBStrokeColor(context, 0, 0, 1, 1);
+			CGContextMoveToPoint(context, 0, -kTickLength);
+			CGContextAddLineToPoint(context, 0, kAxesLength);
+			CGContextDrawPath(context, kCGPathStroke);
+
+			// Paint the x-axis tick marks in red.
+			CGContextSetRGBStrokeColor(context, 1, 0, 0, 1);
+			for(i = 0; i < 2 ; i++)
+			{
+				for(t=0.; t < kAxesLength ; t += kTickDistance){
+					CGContextMoveToPoint(context, t, -tickLength);
+					CGContextAddLineToPoint(context, t, tickLength);
+				}
+				CGContextDrawPath(context, kCGPathStroke);
+				CGContextRotateCTM(context, M_PI/2.);
+				// Paint the y-axis tick marks in blue.
+				CGContextSetRGBStrokeColor(context, 0, 0, 1, 1);
+			}
+			drawPoint(context, CGPointZero);
+			CGContextRestoreGState(context);
+		}
+
+		void drawPoint(CGContextRef context, CGPoint p)
+		{
+			// work with the current context, associated with view
+			NSGraphicsContext ccontext = NSGraphicsContext.CurrentContext;
+			CGContext context = NSGraphicsContext.CurrentContext.GraphicsPort;
+
+			CGContextSaveGState(context);
+			// Opaque black.
+			CGContextSetRGBStrokeColor(context, 0, 0, 0, 1);
+			CGContextSetLineWidth(context, 5);
+			CGContextSetLineCap(context, kCGLineCapRound);
+			CGContextMoveToPoint(context, p.x, p.y);
+			CGContextAddLineToPoint(context, p.x, p.y);
+			CGContextStrokePath(context);
+			CGContextRestoreGState(context);
+
+
+		}
+
 	}
 }
 
